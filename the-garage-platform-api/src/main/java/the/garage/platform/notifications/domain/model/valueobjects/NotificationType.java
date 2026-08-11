dfb205eed376efdd6687cc39e028b802d0548a91
Alonso-Higa <alonsohk25@gmail.com>
2026-08-11 15:54:53 -0500

    feat: add value objects

---
+++ the-garage-platform-api/src/main/java/the/garage/platform/notifications/domain/model/valueobjects/NotificationType.java
@@ -0,0 +1,12 @@
+package the.garage.platform.notifications.domain.model.valueobjects;
+
+public enum NotificationType {
+    EMAIL_VERIFICATION,
+    PASSWORD_RECOVERY,
+    SUBSCRIPTION_CREATED,
+    SUBSCRIPTION_RENEWED,
+    PACK_PURCHASED,
+    LISTING_SOLD,
+    TRADE_COMPLETED,
+    SYSTEM
+}
